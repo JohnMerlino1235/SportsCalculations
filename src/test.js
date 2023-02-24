5ef20c4371f3d4d9de0bfae3b7fe261b152afabf
JohnMerlino1235 <john.merlino1235@gmail.com>
2023-02-23 20:49:24 -0500

    2nd commit test

--- src/test.js
+++ src/test.js
@@ -1,19 +1,23 @@
 const fdClientModule = require('fantasydata-node-client');
 const keys = {
    'CBBv3StatsClient': 'baad666f057d4581beaad950dc119ca8',
-   'CBBv3ScoresClient': 'baad666f057d4581beaad950dc119ca8'
+   'CBBv3ScoresClient': 'baad666f057d4581beaad950dc119ca8',
+   'NFLv3ProjectionsClient': 'baad666f057d4581beaad950dc119ca8',
 };
 
 const CBBClient = new fdClientModule(keys);
-// this.CBBv2Client = new CBBv2Client(apiKeys.CBBv2Client);
-// this.CBBv3ScoresClient = new CBBv3ScoresClient(apiKeys.CBBv3ScoresClient);
-// this.CBBv3StatsClient = new CBBv3StatsClient(apiKeys.CBBv3StatsClient);
-// this.CBBv3OddsClient = new CBBv3OddsClient(apiKeys.CBBv3OddsClient);
+// const CBBv3ScoresClient = require('./CBB/CBBv3Scores');
+// const CBBv3StatsClient = require('./CBB/CBBv3Stats');
+// const CBBv3OddsClient = require('./CBB/CBBv3Odds');
 
+
+// Teams and Stadiums
+// NOT GOOD
 CBBClient.CBBv3ScoresClient.getLeagueHierarchyPromise()
     .then((resp => {
         console.log(resp);
     }))
     .catch((err) => {
         console.log('error', err);
-    });
+});
+
